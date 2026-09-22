faf6497d4707846179004f8743d9dcdd739a5cd5
zoranstankovic <6569042+zoranstankovic@users.noreply.github.com>
2026-09-22 17:27:44 +0200

    chore: name the app JobTrail

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,7 +1,7 @@
 import RootLayout from '@/layouts/RootLayout.vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'JobTrail';
 
 void createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'JobTrail') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
